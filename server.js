60591dbdedbe515c2f80c5d640b0c97b0246b048
orggue <jorgechato1@gmail.com>
2015-01-14 13:35:23 +0100

    initial server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var server = express();
 
+var messages = [];
+var responses = [];
+
 server.get('/',function(req, res){
-        res.send('hello world')
+    responses.push(res);
+});
+
+server.get('/:message',function(req, res){
+    messages.push(req.params.message);
+
+    debugger;
+
+    responses.forEach(function(res){
+        res.send(messages + '<script>window.location.reload()</script>');
+    });
+
+    res.send('el mensaje <b>'+ req.params.message + '</b> ha sido enviado');
 });
 
 server.listen(3000);
