7125931cde5ad7d19716af9e870097221cda8f71
orggue <jorgechato1@gmail.com>
2015-01-18 19:15:43 +0100

    all geo

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ server.io.route('envio',function(req){
 //twitter
 var Twitter = require('twitter');
 
-var config = require(__dirname+'/config.json')
+var config = require(__dirname+'/config.json');
 
 var client = new Twitter({
   consumer_key: config.twitter.consumer_key,
@@ -36,32 +36,55 @@ var client = new Twitter({
 });
 
 server.get('/tweet/:hashtag',function(req,res){
+    n = 0;
     //send to all users
     client.stream('statuses/filter', {track: req.params.hashtag}, function(stream) {
-
         stream.on('data', function(tweet) {
+            n++;
+
             if(tweet.coordinates != null){
                 server.io.broadcast('loadGeo',{lng : tweet.coordinates.coordinates[0], lat : tweet.coordinates.coordinates[1]});
-                console.log("COORDINATES\n****CITY: " + tweet.user.location +"\n" + "****COORD: " + tweet.coordinates.coordinates[0] +" / "+ tweet.coordinates.coordinates[1]+"\n\n");
-            }else{
-                if(tweet.user.location != null){
-                    geocoder.geocode(tweet.user.location,function ( err, data ) {
-                        try{
-                            if(data.results[0] != null && data.status == 'OK'){
-                                server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
-                                console.log("LOCATION\n****CITY: " + tweet.user.location +"\n" + "****COORD: " + data.results[0].geometry.location.lng +" / "+ data.results[0].geometry.location.lat+"\n\n");
-                            }
-                        }catch(err){}
-                    });
-                }else{
-                    if(tweet.place != null){
-                        server.io.broadcast('loadGeo',{lng : tweet.place.bounding_box.coordinates[0], lat : tweet.place.bounding_box.coordinates[1]});
-                        console.log("PLACES\n****CITY: " + tweet.user.location +"\n" + "****COORD: " + tweet.place.bounding_box.coordinates[0][0][0] +" / "+ tweet.place.bounding_box.coordinates[0][0][1]+"\n\n");
-                    }else{
-                        console.log("OTHER\n\n")
-                    }
-                }
+                console.log("COORDINATES\n" + tweet.coordinates.coordinates[0] +"/"+ tweet.coordinates.coordinates[1]+"\n\n");
             }
+
+            if(tweet.location != null){
+                geocoder.geocode(tweet.location,function ( err, data ) {
+                    try{
+                        server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
+                        console.log("TWEET LOCATION\n" + tweet.coordinates.coordinates[0] +"/"+ tweet.coordinates.coordinates[1]+"\n\n");
+                    }catch(err){
+                        console.log(err + " #" + tweet.location);
+                    }
+                });
+            }
+
+            if(tweet.place != null){
+                geocoder.geocode(tweet.place.name +", "+ tweet.place.country,function ( err, data ) {
+                    try{
+                        server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
+                        console.log("PLACES 2\n" + data.results[0].geometry.location.lng+ "/" + data.results[0].geometry.location.lat+"\n\n");
+                    }catch(err){
+                        console.log(err + " #" + tweet.place.full_name);
+                    }
+                });
+            }
+
+            if(tweet.place != null){
+                server.io.broadcast('loadGeo',{lng : tweet.place.bounding_box.coordinates[0][0][0], lat : tweet.place.bounding_box.coordinates[0][0][1]});
+                console.log("PLACES\n" + tweet.place.bounding_box.coordinates[0][0][0] +"/"+ tweet.place.bounding_box.coordinates[0][0][1]+"\n\n");
+            }
+
+            if(tweet.user.location != null){
+                geocoder.geocode(tweet.user.location,function ( err, data ) {
+                    try{
+                        server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
+                        console.log("USER LOCATION\n: " + tweet.user.location +"\n" + "COORD: " + data.results[0].geometry.location.lng +"/"+ data.results[0].geometry.location.lat+"\n\n");
+                    }catch(err){
+                        console.log(err + " @" + tweet.user.name);
+                    }
+                });
+            }
+            console.log("OTHER"+n+"\n\n")
         });
 
     });
