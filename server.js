2d78c700553c799b1e42d9eade2c8940d0a064d6
orggue <jorgechato1@gmail.com>
2015-01-18 20:54:08 +0100

    new structure (modules)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express.io'),
     swig = require('swig'),
-    _ = require('underscore'),
-    geocoder = require('geocoder');
+    Twitter = require('twitter'),
+    _ = require('underscore');
 var server = express();
 server.http().io();
 
@@ -13,19 +13,7 @@ server.engine('html',swig.renderFile);
 server.set('view engine','html');
 server.set('views',__dirname + '/app/views');
 
-server.get('/',function(req, res){
-    res.render('index');
-});
-
-server.io.route('envio',function(req){
-    req.io.emit('recivo', {
-        message : 'serverReady'
-    });
-});
-
 //twitter
-var Twitter = require('twitter');
-
 var config = require(__dirname+'/config.json');
 
 var client = new Twitter({
@@ -35,58 +23,20 @@ var client = new Twitter({
   access_token_secret: config.twitter.access_token_secret
 });
 
-server.get('/tweet/:hashtag',function(req,res){
-    n = 0;
-    //send to all users
-    client.stream('statuses/filter', {track: req.params.hashtag}, function(stream) {
-        stream.on('data', function(tweet) {
-            n++;
+//Map
+var hashtagMap = require(__dirname + '/app/map/twitter');
 
-            if(tweet.coordinates != null){
-                server.io.broadcast('loadGeo',{lng : tweet.coordinates.coordinates[0], lat : tweet.coordinates.coordinates[1]});
-                console.log("COORDINATES\n" + tweet.coordinates.coordinates[0] +"/"+ tweet.coordinates.coordinates[1]+"\n\n");
-            }
+hashtagMap(server,config,client);
 
-            if(tweet.location != null){
-                geocoder.geocode(tweet.location,function ( err, data ) {
-                    try{
-                        server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
-                        console.log("TWEET LOCATION\n" + tweet.coordinates.coordinates[0] +"/"+ tweet.coordinates.coordinates[1]+"\n\n");
-                    }catch(err){
-                        console.log(err + " #" + tweet.location);
-                    }
-                });
-            }
 
-            if(tweet.place != null){
-                geocoder.geocode(tweet.place.name +", "+ tweet.place.country,function ( err, data ) {
-                    try{
-                        server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
-                        console.log("PLACES 2\n" + data.results[0].geometry.location.lng+ "/" + data.results[0].geometry.location.lat+"\n\n");
-                    }catch(err){
-                        console.log(err + " #" + tweet.place.full_name);
-                    }
-                });
-            }
 
-            if(tweet.place != null){
-                server.io.broadcast('loadGeo',{lng : tweet.place.bounding_box.coordinates[0][0][0], lat : tweet.place.bounding_box.coordinates[0][0][1]});
-                console.log("PLACES\n" + tweet.place.bounding_box.coordinates[0][0][0] +"/"+ tweet.place.bounding_box.coordinates[0][0][1]+"\n\n");
-            }
+server.get('/',function(req, res){
+    res.render('index');
+});
 
-            if(tweet.user.location != null){
-                geocoder.geocode(tweet.user.location,function ( err, data ) {
-                    try{
-                        server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
-                        console.log("USER LOCATION\n: " + tweet.user.location +"\n" + "COORD: " + data.results[0].geometry.location.lng +"/"+ data.results[0].geometry.location.lat+"\n\n");
-                    }catch(err){
-                        console.log(err + " @" + tweet.user.name);
-                    }
-                });
-            }
-            console.log("OTHER"+n+"\n\n")
-        });
-
+server.io.route('envio',function(req){
+    req.io.emit('recivo', {
+        message : 'serverReady'
     });
 });
 
