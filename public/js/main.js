889f70b77ae5547047e7dd9d392e9a232f7dd31e
orggue <jorgechato1@gmail.com>
2015-01-18 17:05:29 +0100

    tweet in map

--- public/js/main.js
+++ public/js/main.js
@@ -7,13 +7,16 @@ function main(){
     window.io = io.connect();
 
     io.on('connect',function(socket){
-            comsole.log('hi');
-            io.emit('ready');
+            console.log('hi');
+            io.emit('envio');
+    });
+
+    io.on('recivo',function(data){
+        //something
     });
 }
 
 function init() {
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         zoom: 4,
         minZoom: 3,
@@ -25,28 +28,27 @@ function init() {
         styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#0f252e"},{"lightness":17}]}]
     };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Let's also add a marker while we're at it
-    /*var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
-        map: map,
-        title: 'Snazzy!'
-    });*/
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 4, //tamaño
-            strokeWeight: 0, //grosor del borde
-            fillColor: '#1abc9c', //color de relleno
-            fillOpacity:1// opacidad del relleno
-        },
-        map: map
+    // Let's also add a marker
+    io.on('loadGeo', function(data){
+        var tweet = new google.maps.LatLng(data.lat,data.lng);
+
+        var marker = new google.maps.Marker({
+            position : tweet,
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 4, //tamaño
+                strokeWeight: 0, //grosor del borde
+                fillColor: '#1abc9c', //color de relleno
+                fillOpacity:1// opacidad del relleno
+            },/*
+            draggable:true,
+            animation: google.maps.Animation.DROP,*/
+            map: map
+        });
     });
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express.io'),
     swig = require('swig'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    geocoder = require('geocoder');
 var server = express();
 server.http().io();
 
@@ -12,22 +13,58 @@ server.engine('html',swig.renderFile);
 server.set('view engine','html');
 server.set('views',__dirname + '/app/views');
 
-var messages = [];
-var responses = [];
-
 server.get('/',function(req, res){
-    //responses.push(res);
     res.render('index');
 });
 
-server.get('/hashtag/:message',function(req, res){
-    messages.push(req.params.message);
-
-    responses.forEach(function(res){
-        res.send(messages + '<script>window.location.reload()</script>');
+server.io.route('envio',function(req){
+    req.io.emit('recivo', {
+        message : 'serverReady'
     });
+});
 
-    res.send('el mensaje <b>'+ req.params.message + '</b> ha sido enviado');
+//twitter
+var Twitter = require('twitter');
+
+var config = require(__dirname+'/config.json')
+
+var client = new Twitter({
+  consumer_key: config.twitter.consumer_key,
+  consumer_secret: config.twitter.consumer_secret,
+  access_token_key: config.twitter.access_token_key,
+  access_token_secret: config.twitter.access_token_secret
+});
+
+server.get('/tweet/:hashtag',function(req,res){
+    //send to all users
+    client.stream('statuses/filter', {track: req.params.hashtag}, function(stream) {
+
+        stream.on('data', function(tweet) {
+            if(tweet.coordinates != null){
+                server.io.broadcast('loadGeo',{lng : tweet.coordinates.coordinates[0], lat : tweet.coordinates.coordinates[1]});
+                console.log("COORDINATES\n****CITY: " + tweet.user.location +"\n" + "****COORD: " + tweet.coordinates.coordinates[0] +" / "+ tweet.coordinates.coordinates[1]+"\n\n");
+            }else{
+                if(tweet.user.location != null){
+                    geocoder.geocode(tweet.user.location,function ( err, data ) {
+                        try{
+                            if(data.results[0] != null && data.status == 'OK'){
+                                server.io.broadcast('loadGeo',{lng: data.results[0].geometry.location.lng, lat : data.results[0].geometry.location.lat});
+                                console.log("LOCATION\n****CITY: " + tweet.user.location +"\n" + "****COORD: " + data.results[0].geometry.location.lng +" / "+ data.results[0].geometry.location.lat+"\n\n");
+                            }
+                        }catch(err){}
+                    });
+                }else{
+                    if(tweet.place != null){
+                        server.io.broadcast('loadGeo',{lng : tweet.place.bounding_box.coordinates[0], lat : tweet.place.bounding_box.coordinates[1]});
+                        console.log("PLACES\n****CITY: " + tweet.user.location +"\n" + "****COORD: " + tweet.place.bounding_box.coordinates[0][0][0] +" / "+ tweet.place.bounding_box.coordinates[0][0][1]+"\n\n");
+                    }else{
+                        console.log("OTHER\n\n")
+                    }
+                }
+            }
+        });
+
+    });
 });
 
 server.listen(3000);
